d95ea4a2bbf6f7e92f470604e9771f443317fa8d
Jack Meier <jackson.meier423@gmail.com>
2020-10-19 10:35:10 -0700

    deploying

--- config/initDB.js
+++ config/initDB.js
@@ -1,33 +1,11 @@
-// Ensure you have run 'npm install mongodb'
-var MongoClient = require("mongodb").MongoClient;
-
-var username = "user";
-var password = "password";
-var hosts =
-  "iad2-c0-2.mongo.objectrocket.com:53752,iad2-c0-1.mongo.objectrocket.com:53752,iad2-c0-0.mongo.objectrocket.com:53752";
-var database = "smartwalkerDB";
-var options = "?replicaSet=e1ebecbc129847a1b8af53134799f16f";
-var connectionString =
-  "mongodb://" +
-  username +
-  ":" +
-  password +
-  "@" +
-  hosts +
-  "/" +
-  database +
-  options;
-
 module.exports = () => {
-  return MongoClient.connect(connectionString, function (err, db) {
-    if (db) {
-      db.close();
+  return mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/smart-walker",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
     }
-    if (err) {
-      console.log("Error: ", err);
-    } else {
-      console.log("Connected!");
-      process.exit();
-    }
-  });
+  );
 };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const initDb = require("./config/initDb");
 const morgan = require("morgan");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // log all requests to the console in development
