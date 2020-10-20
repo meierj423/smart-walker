9a140acf696ad51ebdbe38b0a2d020acb8cdb4db
Jack Meier <jackson.meier423@gmail.com>
2020-10-20 12:37:41 -0700

    added code to be able to capture user's zip, city, and state

--- client/src/components/TextBox/index.js
+++ client/src/components/TextBox/index.js
@@ -6,18 +6,28 @@ import LocationForm from "../Form";
 class Textbox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { zip: "", city: "" };
+    this.state = { zip: "", city: "", state: "" };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleZipChange = this.handleZipChange.bind(this);
+    this.handleCityChange = this.handleCityChange.bind(this);
+    this.handleStateChange = this.handleStateChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+  handleZipChange(event) {
     this.setState({ zip: event.target.value });
+  }
+  handleCityChange(event) {
+    this.setState({ city: event.target.value });
+  }
+  handleStateChange(event) {
+    this.setState({ state: event.target.value });
   }
 
   handleSubmit(event) {
-    alert("A location was submitted: " + this.state.zip);
+    alert(
+      `Zip: ${this.state.zip} | City: ${this.state.city} | State: ${this.state.state}`
+    );
     event.preventDefault();
   }
   render() {
@@ -37,12 +47,33 @@ class Textbox extends React.Component {
                   style={{ display: this.props.formDisplay }}
                 >
                   <label>
-                    <input
-                      type="text"
-                      value={this.state.zip}
-                      onChange={this.handleChange}
-                      placeholder={this.props.placeholder1}
-                    />
+                    {/* Input for zip code */}
+                    <div className="centeredRow">
+                      <input
+                        type="text"
+                        value={this.state.zip}
+                        onChange={this.handleZipChange}
+                        placeholder={this.props.placeholder1}
+                      />
+                    </div>
+                    {/* Input for city */}
+                    <div className="centeredRow">
+                      <input
+                        type="text"
+                        value={this.state.city}
+                        onChange={this.handleCityChange}
+                        placeholder={this.props.placeholder2}
+                      />
+                    </div>
+                    {/* Input for state */}
+                    <div className="centeredRow">
+                      <input
+                        type="text"
+                        value={this.state.state}
+                        onChange={this.handleStateChange}
+                        placeholder={this.props.placeholder3}
+                      />
+                    </div>
                   </label>
                   <div className="centeredRow">
                     <input type="submit" value="Next" />
