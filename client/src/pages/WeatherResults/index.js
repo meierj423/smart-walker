2292c7ea0dab97bfbe1837c5a5a665d8ffbdf8f5
Jack Meier <jackson.meier423@gmail.com>
2020-10-22 16:46:15 -0700

    added code to render hourly temps on results page

--- client/src/pages/WeatherResults/index.js
+++ client/src/pages/WeatherResults/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./weather.css";
 import Textbox from "../../components/TextBox";
 
 const WEATHER_API_KEY = "3cb3221cf83645ed701a2873e477b9b9";
@@ -7,7 +8,7 @@ const GEOCODE_API_KEY = "AIzaSyArMGDSwWUvndNNSTGCV27GSE8IUwYgiMw";
 class WeatherResults extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
+    this.state = { forecast: [] };
   }
 
   componentDidMount() {
@@ -17,16 +18,18 @@ class WeatherResults extends React.Component {
     // const state = this.props.location.state.state;
     fetch(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${zip}&key=${GEOCODE_API_KEY}`
-    ).then(function (res) {
-      res.json().then(function (data) {
+    ).then((res) => {
+      res.json().then((data) => {
         let lat = data.results[0].geometry.location.lat;
         let lng = data.results[0].geometry.location.lng;
         console.log(`Lat: ${lat} | Lng: ${lng}`);
         fetch(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=${WEATHER_API_KEY}`
-        ).then(function (res) {
-          res.json().then(function (data) {
-            console.log(data);
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=imperial&appid=${WEATHER_API_KEY}`
+        ).then((res) => {
+          res.json().then((data) => {
+            console.log(data.hourly);
+            const forecastData = data.hourly;
+            this.setState({ forecast: forecastData });
           });
         });
       });
@@ -34,19 +37,43 @@ class WeatherResults extends React.Component {
   }
 
   render() {
+    const forecastArr = this.state.forecast;
+    console.log(forecastArr);
     const zip = this.props.location.state.zip;
     const city = this.props.location.state.city;
     const state = this.props.location.state.state;
 
     return (
-      <Textbox
-        title="Forecast"
-        text={`Zip: ${zip} | City: ${city} | State: ${state}`}
-        buttonDisplay="none"
-        btnMsg="Let's Begin"
-        formDisplay="none"
-        orDisplay="none"
-      />
+      <div id="main">
+        <div className="container p-3">
+          <div className="row">
+            <div className="col-sm">
+              <div className="card p-3">
+                <div className="card-body">
+                  <h2 className="card-title"></h2>
+                  <p className="card-subtitle mb-2"> </p>
+                </div>
+                <ul>
+                  {forecastArr.map((hour) => (
+                    <div className="temps" key={hour.dt}>
+                      {hour.temp}
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      // <Textbox
+      //   title="Forecast"
+      //   text={`Zip: ${zip} | City: ${city} | State: ${state}`}
+      //   buttonDisplay="none"
+      //   btnMsg="Let's Begin"
+      //   formDisplay="none"
+      //   orDisplay="none"
+      // />
     );
   }
 }
