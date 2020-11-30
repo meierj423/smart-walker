262ea29a0c75452960ceaec73af171557fc98edd
Jack Meier <jackson.meier423@gmail.com>
2020-11-30 11:53:43 -0800

    addition info links removed due to 404 error

--- client/src/pages/About/about.js
+++ client/src/pages/About/about.js
@@ -14,19 +14,7 @@ function About() {
                 are not impervious to extreme weather conditions and, as owners,
                 we must be mindful of their safety.
               </p>
-              <br />
-              <p>
-                For more information on your dog and hot weather, please click:{" "}
-                <a href="https://www.gopetplan.com/sites/default/files/inline-images/How%20Hot%20Is%20Too%20Hot%20infographic.png">
-                  here
-                </a>
-              </p>
-              <p>
-                For more information on your dog and cold weather, please click{" "}
-                <a href="https://www.gopetplan.com/sites/default/files/inline-images/PETP_2245_HowColdIsTooCold_Infographic_0918.png">
-                  here
-                </a>
-              </p>
+
               <br />
               <p className="note">
                 *Apparent (i.e. feels like) temperatures are used in this
